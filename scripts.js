1b7373d68f5132629e1bda9bf80cee321be3f99e
Derek Swan <107578303+daswando@users.noreply.github.com>
2023-01-19 12:03:29 -0800

    finished project

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
-const side = 16;
-
-
 let gridBox = document.getElementById("gridBox");
 
-for (let i = 0; i < side; i++) {
-    let newRow = createRow();
+buildGrid(16);
 
-    for (let n = 0; n < side; n++) {
-        let newDiv = createElement();
-        newRow.appendChild(newDiv);
+//------------------------------------------ FUNCTIONS -------------------------
+function rebuildGrid() {
+    destroyGrid();
+    buildGrid(getSize());
+}
+function getSize() {
+    return prompt("Please enter a size for the Grid");
+}
+
+function buildGrid (size) {
+    for (let i = 0; i < size; i++) {
+        let newRow = createRow();
+
+        for (let n = 0; n < size; n++) {
+            let newDiv = createElement();
+            newRow.appendChild(newDiv);
+        }
+        gridBox.appendChild(newRow);
     }
-    gridBox.appendChild(newRow);
+}
+function destroyGrid() {
+    while(gridBox.firstChild != null) {
+        gridBox.removeChild(gridBox.lastChild);
+    }
 }
 
 function createRow() {
@@ -22,5 +37,11 @@ function createElement() {
     let newDiv = document.createElement("div");
     newDiv.id = "boxElement"
     newDiv.textContent ="";
+    newDiv.addEventListener("mouseover", (event) => {
+        newDiv.setAttribute("class", "hoverColor");
+        newDiv.addEventListener("transitionend", () => {
+            newDiv.removeAttribute("class", "hoverColor");
+        });
+    })
     return newDiv;
 }
